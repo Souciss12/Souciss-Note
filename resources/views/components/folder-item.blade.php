c67f5bcb42daea98ec9359a9b7205506858f6e26
Souciss12 <a12c4g@gmail.com>
2025-05-20 10:57:54 +0200

    ➕Added a delete button in the arbo next to the folder and the note

--- resources/views/components/folder-item.blade.php
+++ resources/views/components/folder-item.blade.php
@@ -1,10 +1,11 @@
 <div class="folder">
     <div class="folder-header">
         <div class="folder-name">
+            <span class="open-icon bi bi-caret-right"></span>
             <span class="folder-icon">📁</span>
             <span class="folder-name">{{ $folder->name }}</span>
         </div>
-        <span class="open-icon bi bi-caret-right"></span>
+        <a class="btn-delete-folder bi bi-trash-fill"></a>
     </div>
 
     <div class="folder-content" style="display: none;">
@@ -17,9 +18,16 @@
         @endforeach
 
         @foreach ($notes->where('folder_id', $folder->id) as $note)
-            <div class="note" data-note-id="{{ $note->id }}">
-                <span class="file-icon">📄</span>
-                <span class="note-name">{{ $note->title }}</span>
+            <div class="note-header">
+                <div class="note" data-note-id="{{ $note->id }}">
+                    <div class="note-header">
+                        <div>
+                            <span class="file-icon">📄</span>
+                            <span class="note-name">{{ $note->title }}</span>
+                        </div>
+                        <a class="btn-delete-note bi bi-trash-fill"></a>
+                    </div>
+                </div>
             </div>
         @endforeach
     </div>
