047f8f727b65b8b6248edd8da465b167196246f0
Souciss12 <a12c4g@gmail.com>
2025-05-26 15:06:21 +0200

    ➕Added split screen responsive

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -3,20 +3,20 @@
 @section('content')
     <div class="container-fluid g-0 flex-grow-container">
         <div class="row g-0 row-options">
-            <div class="col-2">
+            <div class="col-xl-2 col-md-3 col-4">
                 <div class="search-bg">
                     <x-note-search />
                 </div>
             </div>
-            <div class="col-10 toolbar-bg">
+            <div class="col-xl-10 col-md-9 col-8 toolbar-bg">
                 <x-note-toolbar />
             </div>
         </div>
         <div class="row g-0 row-arbo">
-            <div class="col-2 arbo-bg">
+            <div class="col-xl-2 col-md-3 col-4 arbo-bg">
                 <x-note-arbo :notes="$notes" :folders="$folders" />
             </div>
-            <div class="col-10 gx-4 note-bg">
+            <div class="col-xl-10 col-md-9 col-8 gx-4 note-bg">
                 <x-note-content />
             </div>
         </div>
